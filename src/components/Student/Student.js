e115c9805d96f399ebaa6782679474488789c657
Alma Powell <alma111000@gmail.comgit config --global user.email alma111000@gmail.comgit config --global user.name Alma>
2019-06-04 15:56:25 -0600

    try black diamond

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {}
+    }
   }
 
+  componentDidMount() {
+    return axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({
+          studentInfo: res.data
+        })
+      })
+  }
+
+
   render() {
+    let student = this.state.studentInfo
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{student.first_name} {student.last_name}</h1>
+        <h3>Grade: {student.grade}</h3>
+        <h3>Email: {student.email}</h3>
       </div>
     )
   }
